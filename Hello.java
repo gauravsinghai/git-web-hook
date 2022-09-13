4cd02b2cb4693f28ecfbf3cb526c127a0f5de149
gauravsinghai <javacodezz@gmail.com>
2022-09-14 00:08:24 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
  public static void main(String[] args)
  {
-  System.out.println("Hello! Welcome to Java!");
+  System.out.println("Hello! Welcome to Java and Jenkins");
  }
 
 }
